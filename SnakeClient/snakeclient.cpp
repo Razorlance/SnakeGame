f1df2e51268b56a875a06e995be70c437f8a53a2
AlinaDekk <adekkusheva@inbox.ru>
2022-12-05 14:37:56 +0300

    Added field without kletochki

--- SnakeClient/snakeclient.cpp
+++ SnakeClient/snakeclient.cpp
@@ -1,11 +1,15 @@
 #include "snakeclient.h"
 #include "./ui_snakeclient.h"
 
+#include <QDebug>
+#include <QMessageBox>
+
 SnakeClient::SnakeClient(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::SnakeClient)
 {
     ui->setupUi(this);
+    this->resize(_width * field_width, _height * field_height);
 }
 
 SnakeClient::~SnakeClient()
@@ -13,3 +17,39 @@ SnakeClient::~SnakeClient()
     delete ui;
 }
 
+
+//doesn't work(((((((((
+void SnakeClient::keyPressEvent(QKeyEvent *event)
+{
+    qDebug() << "works2";
+    /*int key = event->key();
+    qDebug() << event->key();
+    qDebug() << "hello";
+
+    if (event->key() == Qt::Key_Up)
+    {
+        qDebug() << "13";
+    }*/
+    switch(event->key())
+        {
+            case Qt::Key_Up:
+                qDebug() << "Key Up pressed";
+                break;
+
+            case Qt::Key_Right:
+                qDebug() << "Key Right pressed";
+                break;
+            case Qt::Key_Down:
+                qDebug() << "Key Down pressed";
+                break;
+
+            case Qt::Key_Left:
+                qDebug() << "Key Left pressed";
+                break;
+
+            /*default:
+                qDebug() << "Key pressed:" << event->key();
+                break;*/
+        }
+
+}
--- SnakeClient/snakeclient.h
+++ SnakeClient/snakeclient.h
@@ -2,6 +2,8 @@
 #define SNAKECLIENT_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
+#include <QWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class SnakeClient; }
@@ -15,7 +17,16 @@ public:
     SnakeClient(QWidget *parent = nullptr);
     ~SnakeClient();
 
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+
 private:
     Ui::SnakeClient *ui;
+
+    // размер поля
+    static const int _width = 25; // размеры точек
+    static const int _height = 25;
+    static const int field_width = 25; // количество точек
+    static const int field_height = 25;
 };
 #endif // SNAKECLIENT_H
