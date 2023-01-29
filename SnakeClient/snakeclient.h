8c3cfda9465c9cfaffdce1129e144481d50ba324
RomanGorelsky <regorelskiy@edu.hse.ru>
2023-01-29 22:39:47 +0300

    Size modified

--- SnakeClient/snakeclient.h
+++ SnakeClient/snakeclient.h
@@ -40,7 +40,7 @@ private:
 
     // The size of a field
     static const int _width = 25; // The size of points
-    static const int _height = 25;
+    static const int _height = 24;
     static const int _field_width = 25; // The number of points
     static const int _field_height = 25;
     static const int _delay = 150;
