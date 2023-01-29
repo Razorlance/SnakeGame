9fbdfec5824fd82d6555401ac79d0b7bee9376ce
RomanGorelsky <regorelskiy@edu.hse.ru>
2023-01-29 18:27:29 +0300

    QInputDialog modified

--- SnakeClient/snakeclient.cpp
+++ SnakeClient/snakeclient.cpp
@@ -6,49 +6,30 @@
 #include <QInputDialog>
 #include <QDir>
 #include <QSize>
-#include <SFML/Graphics.hpp>
 
 SnakeClient::SnakeClient(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::SnakeClient)
-{
-    // create the window
-        sf::RenderWindow window(sf::VideoMode(600, 600), "SFML WORKS!!!");
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::Green);
-
-        // run the program as long as the window is open
-        while (window.isOpen())
-        {
-            // check all the window's events that were triggered since the last iteration of the loop
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                // "close requested" event: we close the window
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-
-            // clear the window with black color
-            window.clear(sf::Color::Black);
-
-            // draw everything here...
-            window.draw(shape);
-
-            // end the current frame
-            window.display();
-        }
-
+{   
     ui->setupUi(this);
     this->resize(_width * field_width, _height * field_height);
+    this->setWindowTitle("Snake Game");
     bool ok;
     //QInputDialog d;
     //d.setPos;
-    QString text = QInputDialog::getText(this, tr("QInputDialog::getText()"),
-                                         tr("User name:"), QLineEdit::Normal,
-                                         QDir::home().dirName(), &ok);
-    if (ok && !text.isEmpty())
-        ui->textLabel->setText(text);
+    QInputDialog* startWindow = new QInputDialog();
+    startWindow->setLabelText(tr("Enter your name:"));
+    startWindow->setWindowTitle(tr("Start"));
+    startWindow->setTextEchoMode(QLineEdit::Normal);
+    startWindow->adjustSize();
+    startWindow->move(QApplication::desktop()->screen()->rect().center() - startWindow->rect().center());
+    QString text = "";
+    if(startWindow->exec() == QDialog::Accepted)
+        text = startWindow->textValue();
+
+    if (text.isEmpty())
+        text = "Player 1";
+    ui->textLabel->setText(text);
 }
 
 SnakeClient::~SnakeClient()
@@ -91,5 +72,18 @@ void SnakeClient::keyPressEvent(QKeyEvent *event)
                 qDebug() << "Key pressed:" << event->key();
                 break;*/
         }
+
+}
+
+void SnakeClient::initiateGame()
+{
+    stillGame = true;
+    dots.resize(3);
+
+    for (size_t i = 0; i < dots.size(); i++)
+    {
+        dots[i].rx() = dots.size() - i;
+        dots[i].ry() = 0;
+    }
 
 }
--- SnakeClient/snakeclient.h
+++ SnakeClient/snakeclient.h
@@ -1,12 +1,18 @@
 #ifndef SNAKECLIENT_H
 #define SNAKECLIENT_H
 
+#include <QDesktopWidget>
+#include <QApplication>
 #include <QMainWindow>
 #include <QKeyEvent>
 #include <QWidget>
+#include <QScreen>
+#include <QVector>
+#include <QPoint>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class SnakeClient; }
+using namespace std;
 QT_END_NAMESPACE
 
 class SnakeClient : public QMainWindow
@@ -23,10 +29,16 @@ protected:
 private:
     Ui::SnakeClient *ui;
 
-    // размер поля
-    static const int _width = 25; // размеры точек
+    // The size of a field
+    static const int _width = 25; // The size of points
     static const int _height = 25;
-    static const int field_width = 25; // количество точек
+    static const int field_width = 25; // The number of points
     static const int field_height = 25;
+
+    bool stillGame;
+
+    QVector<QPoint> dots;
+
+    void initiateGame();
 };
 #endif // SNAKECLIENT_H
