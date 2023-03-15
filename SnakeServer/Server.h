ec8ec20745c26bda5143b65cd3da9d969b797ab3
Denis Kulakov <kulakovdenis.work@gmail.com>
2023-03-15 15:14:22 +0300

    Fixes with data transmission

--- SnakeServer/Server.h
+++ SnakeServer/Server.h
@@ -3,6 +3,7 @@
 
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QTime>
 #include <QVector>
 
 #include "snake.h"
@@ -14,19 +15,34 @@ class Server : public QTcpServer
     Server();
     QTcpSocket *socket;
 
+   protected:
+    void timerEvent(QTimerEvent *event) override;
+
    private:
     QMap<qintptr, QTcpSocket *> Sockets;
     QMap<qintptr, QVector<QPoint>> _Dots;
     QSet<qintptr> _count;
+    QPoint _fruitPos;
+
+    int _timer;
+    static const int _delay = 1000;
+    static const int _width = 25;  // The size of points
+    static const int _height = 24;
+    static const int _field_width = 25;  // The number of points
+    static const int _field_height = 25;
 
     QVector<QPoint> convertToDots(QStringList &str);
     QString convertToString(QVector<QPoint> &dots);
     QByteArray Data;
     void SendToClient(QString str);
     void SendEnemyCoordinates();
+    bool checkBoundary();
+    void locateFruit();
+    void initiateGame();
+    void endGame();
 
    public slots:
-    void incomingConnection(qintptr SocketDescriptor);
+    void incomingConnection(qintptr SocketDescriptor) override;
     void slotReadyRead();
 };
 
--- SnakeServer/server.cpp
+++ SnakeServer/server.cpp
@@ -1,3 +1,4 @@
+
 #include "server.h"
 
 Server::Server()
@@ -8,15 +9,39 @@ Server::Server()
         qDebug() << "Error";
 }
 
+void Server::timerEvent(QTimerEvent *event)
+{
+    //    qDebug() << "Tick" << _stillGame << _await;
+    //    if (_stillGame & _await)
+    //    {
+    //        eatFruit();
+    //        move();
+    //        checkBoundary();
+    //        _await = false;
+    //        this->repaint();
+    //    }
+    //    SendToServer(_homeDots);
+    if (checkBoundary())
+    {
+        if (_count.size() == Sockets.size())
+        {
+            qDebug() << "Continue the game";
+            SendEnemyCoordinates();
+            SendToClient("c 1");
+            _count.clear();
+        }
+    }
+    else
+        endGame();
+}
+
 void Server::SendToClient(QString str)
 {
     //    out << str;
     //    for (auto &socket : Sockets.values())
     //        socket->write(str.toLocal8Bit());
     //    for (int i = 0; i < Sockets.size(); i++)
-
     //        Sockets[i]->write(str.toLocal8Bit());
-
     //    for (QTcpSocket *i : Sockets)
     //    {
     //        Data.clear();
@@ -25,6 +50,7 @@ void Server::SendToClient(QString str)
     //        out << str;
     //        i->write(Data);
     //    }
+
     for (QMap<qintptr, QTcpSocket *>::Iterator it = Sockets.begin();
          it != Sockets.end(); it++)
     {
@@ -58,6 +84,70 @@ void Server::SendEnemyCoordinates()
     }
 }
 
+bool Server::checkBoundary()
+{
+    for (QMap<qintptr, QVector<QPoint>>::Iterator it = _Dots.begin();
+         it != _Dots.end(); it++)
+    {
+        if (it.value().size() > 4)
+        {
+            for (size_t i = 1; i < it.value().size(); i++)
+            {
+                if (it.value()[0] == it.value()[i])
+                {
+                    return false;
+                }
+            }
+        }
+        if (it.value()[0].x() < 0)
+        {
+            return false;
+        }
+        if (it.value()[0].x() == _field_width)
+        {
+            return false;
+        }
+        if (it.value()[0].y() < 0)
+        {
+            return false;
+        }
+        if (it.value()[0].y() == _field_height)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Server::locateFruit()
+{
+    QTime time = QTime::currentTime();
+    srand((uint)time.msec());
+
+    _fruitPos.rx() = rand() % _width;
+    _fruitPos.ry() = rand() % _height;
+    SendToClient("f " + QString::number(_fruitPos.rx()) + " " +
+                 QString::number(_fruitPos.ry()));
+}
+
+void Server::initiateGame()
+{
+    qDebug() << "Starting the game...";
+    qDebug() << "Creating a timer...";
+    _timer = startTimer(_delay);
+    qDebug() << "Locating fruit...";
+    locateFruit();
+    //        for (qintptr &socket : Sockets.keys())
+    qDebug() << "Sending command to start..";
+    SendToClient("r 1");
+}
+
+void Server::endGame()
+{
+    SendToClient("e");
+    killTimer(_timer);
+}
+
 QVector<QPoint> Server::convertToDots(QStringList &str)
 {
     QVector<QPoint> dots(2);
@@ -88,13 +178,7 @@ void Server::incomingConnection(qintptr SocketDescriptor)
     qDebug() << "Client connected" << SocketDescriptor;
     this->nextPendingConnection();
     if (Sockets.size() == 2)
-    {
-        qDebug() << "Starting the game...";
-
-        qDebug() << "Sending command to start...";
-        //        for (qintptr &socket : Sockets.keys())
-        SendToClient("r 1");
-    }
+        initiateGame();
 }
 
 void Server::slotReadyRead()
@@ -115,16 +199,10 @@ void Server::slotReadyRead()
         {
             _Dots[socket->socketDescriptor()] = convertToDots(L);
             _count.insert(socket->socketDescriptor());
-            SendEnemyCoordinates();
+            //            SendEnemyCoordinates();
         }
     }
     else
         qDebug() << "Error";
     //    SendToClient("c 1");
-    if (_count.size() == Sockets.size())
-    {
-        qDebug() << "Continue the game";
-        SendToClient("c 1");
-        _count.clear();
-    }
 }
